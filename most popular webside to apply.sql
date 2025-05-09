9ddb58d2c2e9bfd29aa81f5997fcc74e9ced4fc2
kondzio7a <118897146+kondzio7a@users.noreply.github.com>
2025-05-09 15:02:57 +0200

    Delete most popular webside to apply.sql

--- most popular webside to apply.sql
+++
@@ -1,5 +0,0 @@
-select regexp_replace(job_via, '^via\s+','') as Portal_with_offers, count(regexp_replace(job_via, '^via\\s+','')) as succesfull_application_count
-from job_postings_fact
-group by Portal_with_offers
-order by succesfull_application_count desc
---limit 10
