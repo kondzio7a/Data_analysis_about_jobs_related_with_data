321cfbe1ca6ac2e6b04cf5bc062b1bd5cc50b3c5
kondzio7a <118897146+kondzio7a@users.noreply.github.com>
2025-05-09 15:02:24 +0200

    Delete top_moth_job_posted.sql

--- top_moth_job_posted.sql
+++
@@ -1,5 +0,0 @@
-select extract(month from job_posted_date) as Month,count (extract(Month from job_posted_date)) as count_month
-from job_postings_fact 
-where extract(year from job_posted_date) = 2023
-group by month
-order by month 
